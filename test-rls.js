44638e471bbfc06f9843c3eaad33406ef4eb0e77
a0920238599-collab <a0920238599@gmail.com>
2026-07-07 15:25:07 +0800

    Delete test-rls.js

--- test-rls.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
-dotenv.config();
-const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
-async function run() {
-  const { data } = await supabase.rpc('exec_sql', { sql_string: 'SELECT policyname FROM pg_policies WHERE tablename = \'system_settings\'' });
-  console.log(data);
-}
-run();
